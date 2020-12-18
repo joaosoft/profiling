3c00ddd8f30c352d26a46f7e00af37f97f1836f4
João Ribeiro <joao.ribeiro@foursource.pt>
2020-12-18 07:45:20 +0000

    docs

--- web/server.go
+++ web/server.go
@@ -2,8 +2,11 @@ package web
 
 import (
 	"fmt"
+	"log"
 	"net/http"
+	"os/exec"
 	"profiling/web/routes"
+	"runtime"
 )
 
 func init() {
@@ -13,7 +16,25 @@ func init() {
 func Start() {
 	routes.RegisterRoutes(mux)
 
-	fmt.Printf("web server started at http://localhost:%d\n", HttpWebServerPort)
+	url := fmt.Sprintf("http://localhost:%d", HttpWebServerPort)
+	fmt.Printf("web server started at %s\n", url)
+
+	if err := showUrl(url); err != nil {
+		log.Println(fmt.Sprintf("cannot start browser: %v", err))
+	}
+
 	http.ListenAndServe(fmt.Sprintf(":%d", HttpWebServerPort), mux)
 }
 
+func showUrl(url string) error {
+	switch runtime.GOOS {
+	case "linux":
+		return exec.Command("xdg-open", url).Start()
+	case "windows":
+		return exec.Command("rundll32", "url.dll,FileProtocolHandler", url).Start()
+	case "darwin":
+		return exec.Command("open", url).Start()
+	default:
+		return fmt.Errorf("unsupported platform")
+	}
+}
